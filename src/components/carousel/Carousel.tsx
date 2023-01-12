bbc48930a9d6a9d83923842c595a4e1f838ab2ec
Jordan Landry <jordanlandry1999@gmail.com>
2023-01-12 15:41:55 -0500

    Fixed sizing and styling of the carousel buttons

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import { ChevronBarContract, ChevronLeft, ChevronRight, Translate } from "react-bootstrap-icons";
+import {
+  ChevronBarContract,
+  ChevronCompactLeft,
+  ChevronCompactRight,
+  ChevronLeft,
+  ChevronRight,
+  Translate,
+} from "react-bootstrap-icons";
 import useWidth from "../../hooks/useWidth";
 
 type Props = {
   children: React.ReactNode;
   gap?: number;
   itemsToShow?: { s: number; m: number; l: number } | number;
-  outsidePadding?: number;
+  extraButtonStyles?: React.CSSProperties;
 };
 
-export default function Carousel({ children, itemsToShow = 3, gap = 10, outsidePadding = 40 }: Props) {
+export default function Carousel({ children, itemsToShow = 3, gap = 10, extraButtonStyles }: Props) {
   const width = useWidth();
-  const breakPoints = { s: 0, m: 768, l: 1024 };
+  const breakPoints = { m: 768, l: 1024 };
+
+  const outsidePadding = parseInt(
+    getComputedStyle(document.documentElement).getPropertyValue("--outside-padding").replace("px", "")
+  );
 
   const [scrollIndex, setScrollIndex] = useState(0);
   const [widthToScroll, setWidthToScroll] = useState(width / 3);
@@ -26,40 +37,67 @@ export default function Carousel({ children, itemsToShow = 3, gap = 10, outsideP
     else numberOfItemsToShow = itemsToShow.l;
   }
 
-  const height = wrapperRef.current?.children[0].getBoundingClientRect().height!;
+  const [itemWidth, setItemWidth] = useState(width - outsidePadding * 2 - gap * (numberOfItemsToShow - 1));
+  const [buttonHeight, setButtonHeight] = useState(wrapperRef.current?.children[0].getBoundingClientRect().height!);
+  // useEffect(() => {
+  //   if (wrapperRef.current === null) return;
+  //   // setButtonHeight(wrapperRef.current.children[0].getBoundingClientRect().height!);
+  //   // setItemWidth(wrapperRef.current.children[0].getBoundingClientRect().width!);
+  // }, [wrapperRef.current]);
 
+  useEffect(() => {
+    if (!wrapperRef.current) return;
+
+    let w = (width - outsidePadding * 2 - gap * (numberOfItemsToShow - 1)) / numberOfItemsToShow;
+
+    // Update the width
+    setWidthToScroll(w * numberOfItemsToShow);
+
+    for (let i = 0; i < wrapperRef.current.children.length; i++) {
+      const child = wrapperRef.current.children[i];
+      // Set the width of the children
+      child.children[0].setAttribute("width", w + "px");
+    }
+
+    setButtonHeight(wrapperRef.current.children[0].getBoundingClientRect().height!);
+  }, [width]);
+
+  // -4 everywhere because for some reason the height of the a tag is 4px larger than the image inside it, there is no padding or margin on the a tag so I don't know why this is happening
   const BUTTON_STYLES: React.CSSProperties = {
-    height: height! - 4 + "px",
+    ...extraButtonStyles,
+    cursor: "pointer",
+    height: buttonHeight! - 4 + "px",
     position: "absolute",
     display: "flex",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
     zIndex: 2,
+    width: outsidePadding - gap,
+    fontSize: "2rem",
   };
-
-  console.log(scrollIndex);
 
   return (
     <div>
-      <div style={{ ...BUTTON_STYLES, left: 0 }} onClick={() => setScrollIndex((prev) => prev - 1)}>
-        <ChevronLeft />
+      <div className="" style={{ ...BUTTON_STYLES, left: 0 }} onClick={() => setScrollIndex((prev) => prev - 1)}>
+        <ChevronCompactLeft />
       </div>
       <div
         ref={wrapperRef}
         style={{
           display: "flex",
           gap: `${gap}px`,
-          transform: `translateX(${widthToScroll * -scrollIndex}px)`,
+          transform: `translateX(calc(${scrollIndex * -100}% - ${scrollIndex * gap}px))`,
           transition: `0.5s`,
         }}
       >
         {children}
       </div>
       <div
-        style={{ ...BUTTON_STYLES, right: 0, transform: `translateY(${-height}px)` }}
+        className=""
+        style={{ ...BUTTON_STYLES, right: 0, transform: `translateY(${-buttonHeight}px)` }}
         onClick={() => setScrollIndex((prev) => prev + 1)}
       >
-        <ChevronRight />
+        <ChevronCompactRight />
       </div>
     </div>
   );
--- src/pages/movieSelect/MovieSelectTab.tsx
+++ src/pages/movieSelect/MovieSelectTab.tsx
@@ -1,10 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
 import nextId from "react-id-generator";
 import Carousel from "../../components/carousel/Carousel";
-import movieData, { MovieType } from "../../data/movie/movieData";
-import useHover from "../../hooks/useHover";
-import useWidth from "../../hooks/useWidth";
 import MovieSelect from "./MovieSelect";
 
 type Props = {
@@ -14,77 +9,16 @@ type Props = {
 
 // Carousel of movies
 export default function MovieSelectTab({ title, movieList }: Props) {
-  // Get how many movies to show based on window width
   const movieElements = Array.from(movieList).map((movie: any) => {
     return <MovieSelect key={nextId()} {...movie} link={`/watch/${movie.id}`} />;
   });
 
-  const [hover, setHover] = useState(false);
-
-  const [buttonWidth, setButtonWidth] = useState(0);
-  const [buttonHeight, setButtonHeight] = useState(0);
-  const movieRef = useRef<HTMLDivElement>(null);
-  const width = useWidth();
-
-  const [scrollIndex, setScrollIndex] = useState(0);
-  const [widthToScroll, setWidthToScroll] = useState(0);
-  const [scrollOffset, setScrollOffset] = useState(0);
-
-  // The width of the next and previous buttons should be the width of the movie shown, that is off screen
-  useEffect(() => {
-    if (movieRef.current === null) return;
-
-    const imageWidth = movieRef.current.children[0].children[0].getBoundingClientRect().width;
-    const outSidePadding = parseInt(
-      getComputedStyle(document.documentElement).getPropertyValue("--outside-padding").replace("px", "")
-    );
-
-    const gap = parseInt(getComputedStyle(movieRef.current).getPropertyValue("gap").replace("px", ""));
-    const numberOfImagesInView = Math.floor(width / imageWidth);
-    const buttonWidth = width - imageWidth * numberOfImagesInView - gap * numberOfImagesInView - outSidePadding;
-
-    setButtonWidth(buttonWidth - gap);
-    setButtonHeight(movieRef.current.children[0].children[0].getBoundingClientRect().height);
-    setWidthToScroll((imageWidth + gap) * numberOfImagesInView);
-    setScrollOffset(gap);
-  }, [movieRef, width]);
-
   return (
-    <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+    <div>
       <h2>{title}</h2>
-      <Carousel outsidePadding={100} itemsToShow={{ s: 1, m: 3, l: 4 }}>
+      <Carousel itemsToShow={{ s: 1, m: 3, l: 4 }} extraButtonStyles={{ borderRadius: "var(--movie-border-radius)" }}>
         {movieElements}
       </Carousel>
-      {/* <div
-        className="movie-select__button pointer"
-        style={{ width: buttonWidth, position: "absolute", left: "0", height: buttonHeight, zIndex: 2 }}
-        onClick={() => setScrollIndex((prev) => prev - 1)}
-      >
-        <ChevronLeft />
-      </div>
-      <div
-        className="movie-select__tab"
-        ref={movieRef}
-        style={{
-          transform: `translateX(${widthToScroll * -scrollIndex - scrollOffset / 2}px)`,
-          transition: "transform 0.5s",
-        }}
-      >
-        {movieElements}
-      </div>
-      <div
-        className="movie-select__button pointer"
-        onClick={() => setScrollIndex((prev) => prev + 1)}
-        style={{
-          width: buttonWidth,
-          position: "absolute",
-          right: "0",
-          height: buttonHeight,
-          transform: `translateY(-${buttonHeight + 4}px)`,
-        }}
-      >
-        <ChevronRight />
-      </div> */}
     </div>
   );
 }
